c3313723017a06adbf89ec41bdaa1cc316aca573
VL <V.lastauskas@protonmail.com>
2019-06-30 22:09:57 +0300

    init

---
+++ HardwareLogger/HardwareLogger/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Threading;
+using OpenHardwareMonitor.Hardware;
+
+namespace HardwareLogger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RuntimeConfig.RequireAdministrator();
+
+            while (true)
+            {
+                GetSystemInfo();
+                Thread.Sleep(1000);
+            }
+        }
+
+        static void GetSystemInfo()
+        {
+            UpdateVisitor updateVisitor = new UpdateVisitor();
+            Computer computer = new Computer
+            {
+                CPUEnabled = true,
+                GPUEnabled = true
+            };
+            computer.Open();
+            computer.Accept(updateVisitor);
+
+            Timer timer = new Timer(Callback, null, 0, 1000);
+
+            void Callback(object state)
+            {
+                Console.Clear();
+                Console.WriteLine("{0}\n", DateTime.Now);
+
+                foreach (IHardware hardware in computer.Hardware)
+                {
+                    hardware.Update();
+
+                    Console.WriteLine("{0}: {1}", hardware.HardwareType, hardware.Name);
+                    foreach (ISensor sensor in hardware.Sensors)
+                    {
+                        // Celsius is default unit
+                        if (sensor.SensorType == SensorType.Temperature)
+                        {
+                            Console.WriteLine("{0}: {1}°C", sensor.Name, sensor.Value);
+                            // Console.WriteLine("{0}: {1}°F", sensor.Name, sensor.Value*1.8+32);
+                        }
+
+                    }
+
+                    Console.WriteLine();
+                }
+            }
+
+            computer.Close();
+        }
+    }
+}
---
+++ HardwareLogger/HardwareLogger/RuntimeConfig.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Security.Principal;
+
+namespace HardwareLogger
+{
+    static class RuntimeConfig
+    {
+        [DllImport("libc")]
+        public static extern uint getuid();
+
+        public static void RequireAdministrator()
+        {
+            try
+            {
+                if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
+                {
+                    using (WindowsIdentity identity = WindowsIdentity.GetCurrent())
+                    {
+                        WindowsPrincipal principal = new WindowsPrincipal(identity);
+                        if (!principal.IsInRole(WindowsBuiltInRole.Administrator))
+                        {
+                            throw new InvalidOperationException("Application must be run as administrator");
+                        }
+                    }
+                }
+                else if (getuid() != 0)
+                {
+                    throw new InvalidOperationException("Application must be run as root");
+                }
+            }
+            catch (Exception ex)
+            {
+                throw new ApplicationException("Unable to determine administrator or root status", ex);
+            }
+        }
+    }
+}
